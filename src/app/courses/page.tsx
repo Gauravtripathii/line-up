4cc6451709730f03d1b6f642a37daaf12845d405
gauravtripathii <gauravtripathii>
2023-10-20 20:29:36 +0530

    courses page ui init

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 function CoursesPage() {
-  return <h1>hi there!</h1>;
+  return (
+    <div className="h-full flex p-10">
+      <div className="add-course border rounded-full h-24 w-24 flex items-center justify-center hover:scale-125 transition ease-in-out cursor-pointer">+</div>
+    </div>
+  );
 }
 
 export default CoursesPage;
