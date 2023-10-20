5a26383863ed6c442ab5041a2fe98e4b7201296e
gauravtripathii <gauravtripathii>
2023-10-20 22:18:45 +0530

    course model

---
+++ src/models/courseModel.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const courseSchema = new mongoose.Schema({
+    code: {
+        type: String,
+        required: [true, "Please provide the course code"],
+        unique: true,
+    },
+    name: {
+        type: String,
+        required: [true, "Please provide a title!"],
+    },
+    description: {
+        type: String,
+        default: "No description available",
+    },
+    profileImage: String,
+    faculty: String,
+    schedule: [
+        {
+            day: {type: String, required: true},
+            time: {type: String, required: true},
+        },
+    ],
+    enrolledUsers: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+    ],
+});
+
+const Course = mongoose.models.courses || mongoose.model("courses", courseSchema);
+export default Course;
